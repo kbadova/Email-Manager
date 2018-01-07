19e4edc53709326f282fc4e5b50a4fb00126d55d
Slavyana Monkova <slaviana_m@mail.bg>
2018-01-07 15:54:30 +0200

    Only one file hello.py is needed

--- Flask-Service/hello.py
+++ Flask-Service/hello.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    if request.method == 'POST':
+        # TODO: sending emails here
+        pass
     return 'Hello, World!'
