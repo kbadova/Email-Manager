eeb2d62988a460d17496d48aa7285d31f4700a4d
Krasimira Badova <krasi@hacksoft.io>
2018-01-27 13:12:32 +0200

    Add url for CoursesListApi

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from .apis import MessagesList, LoginApi
+from .apis import MessagesList, LoginApi, CoursesListApi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^messages/', MessagesList.as_view()),
+    url(r'^courses/', CoursesListApi.as_view()),
+
     url(r'^login/', LoginApi.as_view())
 ]
