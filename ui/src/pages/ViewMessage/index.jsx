fd30b78a17d0b8bddb62008855cd98bd39ccd2f8
Slavyana Monkova <slaviana_m@mail.bg>
2018-01-28 13:16:32 +0200

    Last fixes

--- ui/src/pages/ViewMessage/index.jsx
+++ ui/src/pages/ViewMessage/index.jsx
@@ -1,61 +1,57 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import paths from '../../paths';
-import {fetchMessage} from './actions';
+import { fetchMessage } from './actions';
 
 import '../../base-styles.css';
 import './styles.css';
 
 class ViewMessage extends React.Component {
-	componentDidMount() {
-		const id = parseInt(location.hash.split('#')[1]);
-		this.props.fetchMessage({id: id});
-	}
-	render() {
-		const {message} = this.props;
+  componentDidMount() {
+    const id = parseInt(location.hash.split('#')[1]);
+    this.props.fetchMessage({ id: id });
+  }
+  render() {
+    const { message } = this.props;
 
-		return (
-			<div>
-				<div className="mb-20 mt-10">
-					<div className="breadcrumb">
-						<Link to={paths.messages}>Начало /</Link>
-						<a className="active"> Преглед на съобщение</a>
-					</div>
-					<h1>Преглед на ново съобщение</h1>
-					<div>
-						<span className="recipients">Получатели:</span>
-						{message &&
-							message.students &&
-							message.students.map(rec => {
-								return (
-									<span className="recipient" key="{rec.id}">
-										{rec.email}
-									</span>
-								);
-							})}
-					</div>
-					<div className="view-msg">
-						<h4>{message ? message.subject : ''}</h4>
-						<div>{message ? message.content : ''}</div>
-					</div>
-					<div>
-						<a className="pull-right btn btn-blue">Изпрати</a>
-						<a className="pull-right btn btn-default mr-10">Редактирай</a>
-					</div>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <div className="mb-20 mt-10">
+          <div className="breadcrumb">
+            <Link to={paths.messages}>Начало /</Link>
+            <a className="active"> Преглед на съобщение</a>
+          </div>
+          <h1>Преглед на ново съобщение</h1>
+          <div>
+            <span className="recipients">Получатели:</span>
+            {message &&
+              message.students &&
+              message.students.map(rec => {
+                return (
+                  <span className="recipient" key="{rec.id}">
+                    {rec.email}
+                  </span>
+                );
+              })}
+          </div>
+          <div className="view-msg">
+            <h4>{message ? message.subject : ''}</h4>
+            <div>{message ? message.content : ''}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 const mapStateToProps = state => {
-	return {
-		message: state.fetchMessage.message
-	};
+  return {
+    message: state.fetchMessage.message
+  };
 };
 
 const mapDispatchToProps = {
-	fetchMessage
+  fetchMessage
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ViewMessage);
